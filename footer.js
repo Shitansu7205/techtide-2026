16ec1dd3433eebcc7a7683fcee0dfc9d2e3ef89a
Shitansu7205 <shitansukumargochhayat@gmail.com>
2026-08-17 15:34:10 +0530

    Update the Sitemaps and Redirects

---
+++ footer.js
@@ -0,0 +1,85 @@
+document.write(`
+
+
+
+        <!-- Footer Start -->
+        <div class="container-fluid bg-primary text-light footer wow fadeIn" data-wow-delay="0.1s">
+            <div class="container py-5 px-lg-5">
+                <div class="row g-5">
+                    <div class="col-md-6 col-lg-3">
+                        <p class="section-title text-white h5 mb-4">Address<span></span></p>
+                        <p><i class="fa fa-map-marker-alt me-3"></i>123, Khandagiri Road
+
+Khandagiri, Bhubaneswar - 751019
+
+Odisha,India</p>
+                        <p><i class="fa fa-phone-alt me-3"> <a href="telto:7205121943" style="color:white;"> +917205121943</a></i></p>
+                       <!-- <p><i class="fa fa-envelope me-3"> <a href="mailto:techtide.techtide@gmail.com" style="color:white;">techtide.techtide@gmail.com</a></i></p>  -->
+                        <p><i class="fa fa-envelope me-3"> <a href="mailto:sales@thetechtide.site" style="color:white;">sales@thetechtide.site</a></i></p>
+                        <div class="d-flex pt-2">
+                            <a class="btn btn-outline-light btn-social" href="https://www.youtube.com/@Techtide-bbsr"><i class="fab fa-youtube"></i></a>
+                            <a class="btn btn-outline-light btn-social" href="javascript:void(0);"><i class="fab fa-facebook-f"></i></a>
+                            <a class="btn btn-outline-light btn-social" href="https://www.instagram.com/techtide_community/"><i class="fab fa-instagram"></i></a>
+                            <a class="btn btn-outline-light btn-social" href="https://www.linkedin.com/company/techtide-software-pvt-ltd/"><i class="fab fa-linkedin-in"></i></a>
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-lg-3">
+                        <p class="section-title text-white h5 mb-4">Quick Link<span></span></p>
+                        <a class="btn btn-link" href="about.html">About Us</a>
+                        <a class="btn btn-link" href="servive.html">Services</a>
+                        <a class="btn btn-link" href="team.html">Teams</a>
+                        <a class="btn btn-link" href="pricing.html">Pricing </a>
+                        <a class="btn btn-link" href="contact.html">Contact Us</a>
+                    </div>
+                    <div class="col-md-6 col-lg-3">
+                        <p class="section-title text-white h5 mb-4">Projects<span></span></p>
+                        <div class="row g-2">
+                            <div class="col-4">
+                                <img class="img-fluid" src="pro-11.jpeg" alt="Tech Tide" style="height:66px;">
+                            </div>
+                            <div class="col-4">
+                                <img class="img-fluid" src="pro-1.jpeg" alt="Tech Tide" style="height:66px;">
+                            </div>
+                            <div class="col-4">
+                                <img class="img-fluid" src="pro-6.jpeg" alt="Tech Tide" style="height:66px;">
+                            </div>
+                            <div class="col-4">
+                                <img class="img-fluid" src="pro-2.jpeg" alt="Tech Tide" style="height:66px;">
+                            </div>
+                            <div class="col-4">
+                                <img class="img-fluid" src="pro-4.jpeg" alt="Tech Tide" style="height:66px;">
+                            </div>
+                            <div class="col-4">
+                                <img class="img-fluid" src="pro-13.png" alt="Tech Tide" style="height:66px;">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-lg-3">
+                        <p class="section-title text-white h5 mb-4">Tech Tide<span></span></p>
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d257866.83647966868!2d85.7956564093775!3d20.217824426051084!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6ec57e00c3abedb7%3A0x214b79f6fac653!2sTech%20Tide!5e1!3m2!1sen!2sin!4v1782883281967!5m2!1sen!2sin" width="300" height="180" style="border-radius:10px;" allowfullscreen="" loading="lazy" referrerpolicy="strict-origin-when-cross-origin"></iframe>
+                       
+                    </div>
+                </div>
+            </div>
+            <div class="container px-lg-5">
+                <div class="copyright">
+                    <div class="row">
+                        <div class="col-md-6 text-center text-md-start mb-3 mb-md-0">
+                            &copy; <a class="border-bottom" href="javascript:void(0);">2026</a>, All Right Reserved. 
+							
+							<!--/*** This template is free as long as you keep the footer author’s credit link/attribution link/backlink. If you'd like to use the template without the footer author’s credit link/attribution link/backlink, you can purchase the Credit Removal License from "#". Thank you for your support. ***/-->
+							Designed By <a class="border-bottom" href="javascript:void(0);">TECH TIDE</a>
+                        </div>
+                        <div class="col-md-6 text-center text-md-end">
+
+                                <img src="logos.jpeg" alt="tech tide " style="width:20%" >
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Footer End -->
+
+
+`)
